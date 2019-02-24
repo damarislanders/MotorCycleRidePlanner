cb93dc33b8aa4474a08cfd261063e9f12980940d
Malte Werschy <werschy.m@gmail.com>
2019-02-24 23:31:54 +0000

    Build WAR

--- src/test/java/uk/ac/solent/com600/team3/motorcyclerideplanner/ApplicationTest.java
+++ src/test/java/uk/ac/solent/com600/team3/motorcyclerideplanner/ApplicationTest.java
@@ -7,10 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MotorcycleRidePlannerApplicationTests {
-
+public class ApplicationTest {
     @Test
-    public void contextLoads() {
-    }
-
+    public void contextLoads() { }
 }
